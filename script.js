d380c16bde9e97bbb0d67086e72813581d24f6d7
HulionGH <julia.gramovich@mail.ru>
2021-04-23 17:04:47 +0300

    feat: add update-output func

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*-- FULSCREEN FUNCTION --*/
 document.querySelector('.fullscreen').addEventListener('click', (event) => {
     if (!document.fullscreenElement) {
       document.documentElement.requestFullscreen();
@@ -8,15 +9,25 @@ document.querySelector('.fullscreen').addEventListener('click', (event) => {
     };
   });
 
+ /*-- UPDATE INPUTS FUNCTION --*/
   const inputs = document.querySelectorAll('.filters input');
+  const outputs = document.querySelectorAll('.filters output');
 
   function toUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    let updatedName = this.name + '-result';
+    console.log(updatedName);
+    document.getElementsByName(`${updatedName}`)[0].innerText = `${this.value}`;
   }
 
   inputs.forEach(input => input.addEventListener('change', toUpdate));
   inputs.forEach(input => input.addEventListener('mousemove', toUpdate));
+
+ 
+ 
+
+
 
 
 
