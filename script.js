71612f636a5b7bbe74b58b28745a9d5075fca2a7
HulionGH <julia.gramovich@mail.ru>
2021-04-23 15:42:57 +0300

    feat: add update filter func

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.querySelector('.fullscreen').addEventListener('click', (event) => {
       }
     };
   });
+
+  const inputs = document.querySelectorAll('.filters input');
+
+  function toUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  }
+
+  inputs.forEach(input => input.addEventListener('change', toUpdate));
+  inputs.forEach(input => input.addEventListener('mousemove', toUpdate));
+
+
+
+
