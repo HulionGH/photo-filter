cc871cd841d552b7cca50d251b0002177c3e008c
HulionGH <julia.gramovich@mail.ru>
2021-04-23 13:36:53 +0300

    init: start photo-filter task

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.fullscreen').addEventListener('click', (event) => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    };
+  });
