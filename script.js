9e4f8d00b7494b1aed1484caaf123bfe20b9cadb
HulionGH <julia.gramovich@mail.ru>
2021-04-26 10:34:59 +0300

    add: add images for nextpicbtn

--- script.js
+++ script.js
@@ -17,15 +17,39 @@ document.querySelector('.fullscreen').addEventListener('click', (event) => {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     let updatedName = this.name + '-result';
-    console.log(updatedName);
     document.getElementsByName(`${updatedName}`)[0].innerText = `${this.value}`;
   }
 
   inputs.forEach(input => input.addEventListener('change', toUpdate));
   inputs.forEach(input => input.addEventListener('mousemove', toUpdate));
 
- 
- 
+  /*-- Buttons --*/
+  const buttons = document.querySelectorAll('.btn');
+  const resetBtn = document.querySelector('.btn-reset');
+  
+  document.querySelectorAll('.btn').forEach(btn => btn.addEventListener('click', (element) => {
+    let pressedBtn = element.target;
+    pressedBtn.classList.add('btn-active');
+    }));
+
+
+  /*-- RESET FUNCTION --*/
+  function resetValues () {
+    inputs.forEach(input => {
+      const value = input.name == 'saturate' ? 100 : 0;
+      const suffix = input.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${input.name}`, value + suffix);
+      input.value = value;
+      let updatedName = input.name + '-result';
+      document.getElementsByName(`${updatedName}`)[0].innerText = `${value}`;
+    });
+  }
+  resetBtn.addEventListener('click', resetValues);
+  
+  
+  
+  
+  
 
 
 
